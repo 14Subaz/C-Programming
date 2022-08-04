7f1eb2e4929dfaa7947d3e07802e00b1f77116e4
Subash_S <14subaz@gmail.com>
2022-08-04 09:38:00 +0530

    Basic studies added

---
+++ Learning C/Logical op.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=32; //>=35 and(&&) <=100
+    printf("\nLogical And : %d",(a>=35 && a<=100));
+    printf("\nLogical Or  : %d",(a>=35 || a<=100));
+    printf("\nLogical Not : %d",!(a>=35));
+
+
+    return 0;
+}
+
