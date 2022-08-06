c0fb8a1628e5bfb8c756216f2ec6e4a7050ff139
Subash_S <14subaz@gmail.com>
2022-08-06 12:22:38 +0530

    Double Pointer and triple pointer added

--- Learning C/Pointers.c
+++ Learning C/Pointers.c
@@ -19,19 +19,43 @@ int main()
 
 }
 */
-//Double Pointer*************
+//Double Pointer and triple pointer *************
 //sample program for double pointer
 #include<stdio.h>
 
 int main()
 {
-    int a=10, *p;
+    int a=10;
+    int *p; // Single pointer
+    int **q; // Double pointer
+    int ***r; // Triple pointer
     p=&a; // Address of A.
+
     printf("\n Value of A   : %d", a);
     printf("\n Address of A : %d", &a);
     printf("\n Value of P   : %d", p);
     printf("\n Address of P : %d", &p);
     printf("\n Dereferencing of P : %d", *p);
+
+    printf("\n -----------------------------------");
+
+    q=&p; // Address of **q.
+    printf("\n Value of P   : %d", p);
+    printf("\n Address of p : %d", &p);
+    printf("\n Value of q   : %d", q);
+    printf("\n Address of q : %d", &q);
+    printf("\n Dereferencing of P : %d", **q);
+
+     printf("\n -----------------------------------");
+
+     r=&q; // Address of **q.
+    printf("\n Value of q   : %d", q);
+    printf("\n Address of q : %d", &q);
+    printf("\n Value of r  : %d", r);
+    printf("\n Address of r : %d", &r);
+    printf("\n Dereferencing of r : %d", ***r);
+
+     printf("\n -----------------------------------");
 
     return 0;
 
