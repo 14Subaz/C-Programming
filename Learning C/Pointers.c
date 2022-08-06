02c8776e65511de288eeeb4699d4a9d35d8a19bc
Subash_S <14subaz@gmail.com>
2022-08-06 11:25:41 +0530

    Single pointer added

--- Learning C/Pointers.c
+++ Learning C/Pointers.c
@@ -1,6 +1,26 @@
 // C- Pointers ********************************************************************!important
 
 //sample program
+//Single pointer*************
+/*
+#include<stdio.h>
+
+int main()
+{
+    int a=10, *p;
+    p=&a; // Address of A.
+    printf("\n Value of A   : %d", a);
+    printf("\n Address of A : %d", &a);
+    printf("\n Value of P   : %d", p);
+    printf("\n Address of P : %d", &p);
+    printf("\n Dereferencing of P : %d", *p);
+
+    return 0;
+
+}
+*/
+//Double Pointer*************
+//sample program for double pointer
 #include<stdio.h>
 
 int main()
