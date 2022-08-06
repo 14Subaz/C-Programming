8b82811d7ce031f111297b82a8b84a7fb0ec6022
Subash_S <14subaz@gmail.com>
2022-08-06 10:10:11 +0530

    Recursion Function

--- Learning C/Arrays.c
+++ Learning C/Arrays.c
@@ -234,7 +234,7 @@ int add()
 
     return a+b;
 }
-*/
+*//*
 
 //Return With Argument Function in C
 #include<stdio.h>
@@ -252,4 +252,25 @@ int main()
 int add(int x,int y)
 {
     return x+y;
+}
+*/
+//Recursion Function in C Programming
+#include<stdio.h>
+/*
+    5!
+    1*2*3*4*5
+*/
+
+int factorial(int i) //5
+{
+    if(i<=1){
+        return 1;
+    }
+    return i*factorial(i-1);  //5*4*3*2*1
+}
+
+int main()
+{
+    printf("Factorial : %d",factorial(5));
+    return 0;
 }
