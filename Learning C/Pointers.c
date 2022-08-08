8651ab2dbdf92436d28658a26b9c887dbae6cb15
Subash_S <14subaz@gmail.com>
2022-08-08 09:29:00 +0530

    calloc in pointer added

--- Learning C/Pointers.c
+++ Learning C/Pointers.c
@@ -119,7 +119,7 @@ int main()
     printf("\n Dereferencing of P: %d", *(int *)p);
     return 0;
 }
-*/
+*//*
 //malloc in Pointers
 #include<stdio.h>
 #include<stdlib.h>
@@ -141,7 +141,7 @@ int main()
     {
         printf("Enter a integer : ");
         scanf("%d",ptr+i);
-    }*/
+    }
     for(i=0;i<n;i++)
     {
         printf("%d  ",*(ptr+i));  // ptr+1
@@ -149,3 +149,39 @@ int main()
 
     return 0;
 }
+*/
+
+//calloc in pointer*************
+
+//calloc in Pointers
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    //void* calloc(size_t n,size_t size)
+
+    int i,n;
+    printf("\nEnter The Limit : ");
+    scanf("%d",&n);
+    int *ptr=(int *)calloc(n,sizeof(int));
+
+    if(ptr==NULL)
+    {
+        printf("Memory Not Available ...");
+        exit(1);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        printf("Enter a integer : ");
+        scanf("%d",ptr+i);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        printf("\n%d :  %d  ",&ptr+i,*(ptr+i));
+    }
+
+    return 0;
+}
+
